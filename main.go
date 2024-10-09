207c1f3176819fc151441ac966087842b63b8829
Ito <158491297+itocode21@users.noreply.github.com>
2024-10-09 17:53:33 +0300

    1

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -17,9 +19,44 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/home.html"))
 	tmpl.Execute(w, nil)
 }
+
+func convertLenght(value float64, fromUnit, toUnit string) (float64, error) {
+	switch fromUnit + "to" + toUnit {
+	case "millimeter to centimeter":
+		return (value / 10.0), nil
+	case "millimeter to meter":
+		return (value / 1000.0), nil
+	case "millimeter to inch":
+		return (value / 25.4), nil
+	case "millimeter to foot":
+		return (value / 304.8), nil
+	case "millmeter to yard":
+		return (value / 914.4), nil
+	case "millimeter to mile":
+		return (value / 1609344.0), nil
+	default:
+		return value, fmt.Errorf("not supporte from %s to %s", fromUnit, toUnit)
+	}
+
+}
+
 func LengthHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+		value, _ := strconv.ParseFloat(r.FormValue("value"), 64)
+		formUnit := r.FormValue("FromUnit")
+		toUnit := r.FormValue("toUnit")
+		result, _ := convertLenght(value, formUnit, toUnit)
+		tmpl := template.Must(template.ParseFiles("templates/length.html"))
+		tmpl.Execute(w, map[string]interface{}{
+			"Result": fmt.Sprintf("%.2f", result),
+		})
+		return
+	}
+
 	tmpl := template.Must(template.ParseFiles("templates/length.html"))
 	tmpl.Execute(w, nil)
+
 }
 
 func WeigthHandler(w http.ResponseWriter, r *http.Request) {
--- math.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func metr_to(metr float64) float64 {
-	some_data := metr * 2.2
-	return some_data
-}
